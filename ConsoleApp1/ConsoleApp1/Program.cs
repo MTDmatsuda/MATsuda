aac0accab590a5b360a61242a150d0caf6619453
matsuda <yosaku122630@gmail.com>
2020-07-14 14:34:40 +0900

    bmi

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,9 +6,42 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("あああああ");
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("オプション:" + args[i]);
+            }
+            float height = InputAndParse("身長を入力してください");
 
-            Console.WriteLine("Hello World!");
+            float weight = InputAndParse("体重を入力して下さい");
+
+            float bmi = weight / (height * height);
+            Console.WriteLine("BMIは" + bmi.ToString());
+
         }
+        static float CalculateBMI(float weight, float height)
+        {
+            return weight / (height * height);
+        }
+        static float InputAndParse(string massage)
+        {
+            float bmi = 0f;
+            bool parseSuccess = false;
+            while (!parseSuccess)
+            {
+
+                Console.WriteLine(massage);
+
+                string inputbmi = Console.ReadLine();
+
+                parseSuccess = float.TryParse(inputbmi, out bmi);
+
+                if (!parseSuccess)
+                    Console.WriteLine("エラー");
+            }
+            return bmi;
+        }
+
+
     }
+    
 }
